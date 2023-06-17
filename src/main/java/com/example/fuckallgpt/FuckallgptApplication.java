3744f9ea26851efdbda7938efa793f34db4b2747
Mahmudul Hasan Shafin <mahmud.shafin@bidtrix.com>
2023-06-18 02:58:32 +0600

    initial project setup

---
+++ src/main/java/com/example/fuckallgpt/FuckallgptApplication.java
@@ -0,0 +1,13 @@
+package com.example.fuckallgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuckallgptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuckallgptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fuckallgpt/FuckallgptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fuckallgpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuckallgptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
